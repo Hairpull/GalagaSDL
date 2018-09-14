bec41a70b609696deb7952f700378aa2fa7b9acc
Dustin Beck <hairpull@gmail.com>
2018-09-14 13:21:45 -0700

    Initial Commit

---
+++ SDLTutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDLTutorial
+//
+//  Created by Dustin Beck on 9/14/18.
+//  Copyright © 2018 Dustin Beck. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
